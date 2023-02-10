6ef8c10af755e16a38699caa4c129ffb2f27ca3c
brayanagu <brayanfandino@colegioguanenta.edu.co>
2023-02-10 10:05:30 -0500

    Se creó y se añadió el programa python

---
+++ nota_definitiva_asignatura.py
@@ -0,0 +1,15 @@
+#programa para saber las definitavas de una asignatura
+
+#input
+
+nc = int(input ("digite el valor de nc:" ))
+np = int(input ("digite el valor de np:" ))
+na = int(input ("digite el valor de na:" ))
+au = int(input ("digite el valor de au:" ))
+bi = int(input ("digite el valor de bi:" ))
+
+#processig
+nd = (0.3*nc) + (0.3*np) + (0.1*na) + (0.1*au) + (0.2*bi)
+
+#output
+print("la nota definitiva es de" + str(nd))
